0c1a0a4ff036c565dc46e336d8ea79a62f7e97b3
arkk200 <arkk20000@gmail.com>
2026-01-14 08:04:11 +0900

    Feat - Icon 컴포넌트에 weight props 추가

--- front/src/components/common/Icon.tsx
+++ front/src/components/common/Icon.tsx
@@ -1,26 +1,39 @@
 import { cn } from "@/utils/cn";
 
+type IconProps = {
+  name: string;
+  filled?: boolean;
+  size?: number;
+  className?: string;
+  color?: string;
+  variation?: 'rounded' | 'sharp' | 'outlined';
+  weight?: number;
+};
+
 export default function Icon({
   name,
   filled,
   size,
   className,
   color,
-}: {
-  name: string;
-  filled?: boolean;
-  size?: number;
-  className?: string;
-  color?: string;
-}) {
+  variation = 'outlined',
+  weight = 400,
+}: IconProps) {
+  const getFontVariationSettings = (filled: boolean, weight: number) => {
+    return `'FILL' ${filled ? 1 : 0}, 'wght' ${weight}, 'GRAD' 0, 'opsz' 48`;
+  };
+
   return (
     <span
-      className={cn("material-symbols-outlined", className)}
+      className={cn({
+        "material-symbols-outlined": variation === 'outlined',
+        "material-symbols-rounded": variation === 'rounded',
+        "material-symbols-sharp": variation === 'sharp',
+      }, className)}
+
       style={{
         fontSize: `${size}px`,
-        fontVariationSettings: filled
-          ? "'FILL' 1, 'wght' 400, 'GRAD' 0, 'opsz' 48"
-          : "'FILL' 0, 'wght' 400, 'GRAD' 0, 'opsz' 48",
+        fontVariationSettings: getFontVariationSettings(filled, weight),
         color: color,
       }}
     >
