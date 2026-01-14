df649831ed41f3c6e81e8684954a8c014908ed6c
arkk200 <arkk20000@gmail.com>
2026-01-14 23:45:10 +0900

    Feat - 메인 페이지 제작

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -1,18 +1,17 @@
 import { ReactNode } from "react";
-import "../styles/globals.css"
+import "../styles/globals.css";
 import MSWProvider from "./msw-provider";
 
 // next.js에 font 최적화해 적용하는 기능으로 KoddiUD_OnGothic 폰트 적용해야 할 것 같아요
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-    return (
-        <html lang='ko'>
-            <body className="bg-blue-900">
-                <div className="mx-auto max-w-screen-lg px-5 pt-9 pb-9 text-center">
-                    <MSWProvider>{children}</MSWProvider>
-                </div>
-
-            </body>
-        </html>
-    );
+  return (
+    <html lang="ko">
+      <body className="bg-blue-900">
+        <div className="mx-auto max-w-5xl text-center">
+          <MSWProvider>{children}</MSWProvider>
+        </div>
+      </body>
+    </html>
+  );
 }
--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,13 +1,48 @@
+import Icon from "@/components/common/Icon";
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div className="bg-blue-500 h-screen">
-      <h1 className="text-red-600 text-2xl">메인화면</h1>
-      <ul>
-        <li>식단</li>
-        <li>운동</li>
-        <li>약</li>
-        <li>마이페이지</li>
-      </ul>
+    <div className="bg-blue-500 h-screen flex flex-col items-center justify-center py-15">
+      <div className="flex items-center gap-1">
+        <Image
+          src="/Seesunlogo_240x240.png"
+          width={60}
+          height={60}
+          alt="시선 로고"
+        />
+        <h1 className="text-title-medium text-[#FFDB65]">See:Sun</h1>
+      </div>
+
+      <div className="flex-1 flex flex-col justify-center">
+        <div className="grid-cols-2 grid gap-4">
+          <Card title="식단" icon="fork_spoon" />
+          <Card title="운동" icon="exercise" />
+          <Card title="약" icon="pill" />
+          <Card title="내정보" icon="person" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+// TODO: 나중에 폴더 구조 정해지면 분리하기
+type CardProps = {
+  title: string;
+  icon: string;
+};
+
+function Card({ title, icon }: CardProps) {
+  return (
+    <div className="flex flex-col items-center gap-3 px-10 w-full cursor-pointer rounded-[16px] shadow-100 outline-2 outline-black bg-yellow-500 h-52.5 justify-center">
+      <h2 className="break-keep text-title-small text-blue-900">{title}</h2>
+      <Icon
+        name={icon}
+        size={64}
+        filled
+        className="text-blue-900"
+        variation="rounded"
+      />
     </div>
   );
 }
