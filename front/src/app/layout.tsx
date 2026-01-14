d0c33481a3efe58ffaacd00484a4cbb8a48afe50
이명재 <102217839+arkk200@users.noreply.github.com>
2026-01-14 14:11:14 +0900

    Feat - 디자인 시스템 구성 (#3)
    * Feat-라이브러리 설치 & 라우터 설정 완료
    
    * Feat - 색상, 타이포그래피 구성
    
    * Feat - Button 컴포넌트 제작
    
    * Feat - Icon, MicButton 컴포넌트 제작
    
    * Feat - Input 컴포넌트 제작
    
    * Feat - Icon 컴포넌트에 weight props 추가
    
    * Feat - Checkbox 컴포넌트 제작
    
    * Feat - ProgressBar 컴포넌트 제작
    
    ---------
    
    Co-authored-by: taek99 <gunha1215@gmail.com>

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -1,13 +1,17 @@
 import { ReactNode } from "react";
 import "../styles/globals.css"
 
-export default function RootLayout({children} : {children: ReactNode}){
+
+// next.js에 font 최적화해 적용하는 기능으로 KoddiUD_OnGothic 폰트 적용해야 할 것 같아요
+
+export default function RootLayout({ children }: { children: ReactNode }) {
     return (
         <html lang='ko'>
             <body className="bg-blue-900">
-                 <div className="mx-auto max-w-screen-lg px-5 pt-9 pb-9 text-center">
+                <div className="mx-auto max-w-screen-lg px-5 pt-9 pb-9 text-center">
                     {children}
                 </div>
+
             </body>
         </html>
     );
