ca668df19d1d891b4f3170f5735b30e16609264a
arkk200 <arkk20000@gmail.com>
2026-01-16 13:20:20 +0900

    Fix - SSR 시 MSW 초기화 후 렌더링 되도록 수정

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -1,18 +1,17 @@
 import { ReactNode } from "react";
-import "../styles/globals.css"
+import "../styles/globals.css";
 import MSWProvider from "./msw-provider";
 
 // next.js에 font 최적화해 적용하는 기능으로 KoddiUD_OnGothic 폰트 적용해야 할 것 같아요
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-    return (
-        <html lang='ko'>
-            <body className="bg-blue-500">
-                <div className="mx-auto h-screen max-w-5xl px-5 pt-9 pb-9 text-center">
-                    <MSWProvider>{children}</MSWProvider>
-                </div>
-
-            </body>
-        </html>
-    );
+  return (
+    <html lang="ko">
+      <body className="bg-blue-500">
+        <div className="mx-auto h-screen max-w-5xl px-5 pt-9 pb-9 text-center">
+          <MSWProvider>{children}</MSWProvider>
+        </div>
+      </body>
+    </html>
+  );
 }
