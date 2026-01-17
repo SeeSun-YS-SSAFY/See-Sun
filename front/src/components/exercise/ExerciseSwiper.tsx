b9583b660ae169a98770d4020f88dc2ebb986786
arkk200 <arkk20000@gmail.com>
2026-01-17 17:38:14 +0900

    Fix - ExerciseSwiper 수평 정렬 문제 해결

--- front/src/components/exercise/ExerciseSwiper.tsx
+++ front/src/components/exercise/ExerciseSwiper.tsx
@@ -30,67 +30,63 @@ export default function ExerciseSwiper({ exercises }: ExerciseSwiperProps) {
           onSwiper={(swiper) => (swiperRef.current = swiper)}
           effect={"creative"}
           grabCursor={true}
-          centeredSlides={true}
           slidesPerView="auto"
           direction={"vertical"}
-          spaceBetween={0}
           creativeEffect={{
-            limitProgress: 3,
+            limitProgress: 1,
             prev: {
-              translate: ["0%", "20%", -200],
-              scale: 0.8,
+              translate: ["0%", "-40%", -200],
             },
             next: {
-              translate: ["0%", "-20%", -200],
-              scale: 0.8,
+              translate: ["0%", "40%", -200],
             },
           }}
           modules={[EffectCreative]}
-          className="swiper-container h-full w-full"
-          onTouchEnd={() => {
-            swiperRef.current?.slideToClosest();
-          }}
+          className="swiper-container h-[400px]"
         >
           {exercises.map((exercise) => {
             return (
               <SwiperSlide
                 key={exercise.exercise_id}
-                className="h-[212px]! w-full rounded-[20px] my-auto transition-all duration-300"
+                className="w-full rounded-[20px] my-auto transition-all duration-300"
               >
                 {({ isActive }) => (
-                  <div
-                    className={cn(
-                      isActive
-                        ? "bg-yellow-300 shadow-100 outline-2 -outline-offset-2 outline-black"
-                        : "bg-yellow-700 opacity-80",
-                      "self-stretch h-full w-full rounded-[20px] inline-flex flex-col justify-center items-center gap-2.5 transition-all duration-300"
-                    )}
-                  >
+                  <div className="flex flex-col justify-center h-full">
                     <div
-                      className={cn("relative w-40 h-0", isActive && "h-32")}
+                      className={cn(
+                        isActive
+                          ? "bg-yellow-300 shadow-100 outline-2 -outline-offset-2 outline-black"
+                          : "bg-yellow-700 opacity-80",
+                        "self-stretch w-full rounded-[20px] inline-flex flex-col justify-center items-center gap-2.5 transition-all duration-300 h-[212px]"
+                      )}
                     >
-                      <Image
-                        src={exercise.pictogram_url}
-                        alt={exercise.exercise_name}
-                        fill
-                        className={cn(
-                          "object-contain hidden",
-                          isActive && "block"
-                        )}
-                      />
-                    </div>
+                      <div
+                        className={cn("relative w-40 h-0", isActive && "h-32")}
+                      >
+                        <Image
+                          src={exercise.pictogram_url}
+                          alt={exercise.exercise_name}
+                          fill
+                          className={cn(
+                            "object-contain hidden",
+                            isActive && "block"
+                          )}
+                        />
+                      </div>
 
-                    {/* Text area */}
-                    <div
-                      className={`
+                      {/* Text area */}
+                      <div
+                        className={`
                         text-center
-                        ${isActive
-                          ? "text-black text-3xl font-extrabold"
-                          : "text-gray-900 text-xl font-bold"
+                        ${
+                          isActive
+                            ? "text-black text-3xl font-extrabold"
+                            : "text-gray-900 text-xl font-bold"
                         }
                       `}
-                    >
-                      {exercise.exercise_name}
+                      >
+                        {exercise.exercise_name}
+                      </div>
                     </div>
                   </div>
                 )}
