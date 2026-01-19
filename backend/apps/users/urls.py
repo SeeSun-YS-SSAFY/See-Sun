0c764d1b29ee115ade73ff255805efb35d9029d8
taek99 <gunha1215@gmail.com>
2026-01-19 17:19:57 +0900

    fix:로그인, 회원가입, 마이페이지 검증 완료

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -9,10 +9,10 @@ from .views import (
 app_name = 'users'
 
 urlpatterns = [
-    path('auth/signup', SignupView.as_view(), name='signup'),
-    path('auth/login', LoginView.as_view(), name='login'),
-    path('auth/logout', LogoutView.as_view(), name='logout'),
-    path('auth/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/signup/', SignupView.as_view(), name='signup'),
+    path('auth/login/', LoginView.as_view(), name='login'),
+    path('auth/logout/', LogoutView.as_view(), name='logout'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('auth/oauth/google', GoogleLoginView.as_view(), name='google_login'),
     path('profile/completion/', UserProfileCompletionView.as_view(), name='profile_complete'),
     path('profile/edit/', UserProfileUpdateView.as_view(), name='profile_update'),
