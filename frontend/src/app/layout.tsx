26c626f0d576df3004dad235e0f5b5c0e88bcbd8
arkk200 <arkk20000@gmail.com>
2026-01-23 10:43:02 +0900

    Feat - KoddiUD 폰트 적용

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,30 +1,42 @@
 import { ReactNode } from "react";
 import "../styles/globals.css";
-import MSWProvider from "./msw-provider";
 import Providers from "./providers";
 import GuideAudioPlayer from "@/components/GuideAudioPlayer";
-import localFont from 'next/font/local'
+import localFont from "next/font/local";
 
 // next.js에 font 최적화해 적용하는 기능으로 KoddiUD_OnGothic 폰트 적용해야 할 것 같아요
 const koddiudOnGothic = localFont({
   src: [
     {
-      path: '/KoddiUDOnGothic-Regular.woff',
-      weight: '400',
-      style: 'normal'
+      path: "./fonts/KoddiUDOnGothic-Regular.woff",
+      weight: "400",
+      style: "normal",
     },
-  ]
-})
+    {
+      path: "./fonts/KoddiUDOnGothic-Bold.woff",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./fonts/KoddiUDOnGothic-ExtraBold.woff",
+      weight: "800",
+      style: "normal",
+    },
+  ],
+  display: "swap",
+  preload: true,
+  variable: "--font-koddiud-ongothic",
+});
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="ko">
+    <html lang="ko" className={koddiudOnGothic.variable}>
       <body className="bg-blue-500">
         <div className="mx-auto max-w-5xl px-5 pt-9 pb-9 text-center">
           <Providers>
             <GuideAudioPlayer />
             {children}
-            </Providers>
+          </Providers>
         </div>
       </body>
     </html>
